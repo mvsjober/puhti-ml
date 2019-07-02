a1f652988d7e9e6b03bca985943b81c0360e71f6
Mats Sjöberg <mats.sjoberg@helsinki.fi>
2019-07-02 09:59:27 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PIP=pip3
+# TARGET=~/appl
+TARGET=${WRKDIR}/appl/opt
+
+
+REQFN=$1
+if [ -z "$REQFN" ]; then
+    echo "Usage: $0 REQFN"
+    echo "Example: $0 pkg-1.2.3.txt"
+    echo "where pkg-1.2.3.txt is the requirements.txt file for installing package pkg-1.2.3"
+    exit 1
+fi
+
+PKG=${REQFN/.txt/}
+FROZEN="frozen_${REQFN}"
+
+PREFIX="$TARGET/$PKG"
+if [ -d "$PREFIX" ]; then
+    echo "WARNING: $PREFIX directory already exists"
+fi
+
+echo "Install packages:"
+sed -e 's/^/  /' $REQFN
+echo "to installation prefix: $PREFIX"
+read -p "OK? [Return = OK, Ctrl-C = abort] "
+
+set -o xtrace
+
+mkdir -p $PREFIX
+
+#  --ignore-installed
+$PIP install --no-warn-script-location --prefix $PREFIX -r $REQFN
+PYTHONPATH=${PREFIX}/lib64/python3.6/site-packages:${PREFIX}/lib/python3.6/site-packages \
+          $PIP freeze -r $REQFN > $FROZEN
