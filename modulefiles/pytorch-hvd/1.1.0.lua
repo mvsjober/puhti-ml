0c84048a6ca8cc80ad9f81a37fe297e1c2abd1cd
Mats Sjöberg <mats.sjoberg@csc.fi>
2019-07-26 14:51:06 +0300

    Added family to all modules. Added CONDA activation setenvs.

--- modulefiles/pytorch-hvd/1.1.0.lua
+++ modulefiles/pytorch-hvd/1.1.0.lua
@@ -4,6 +4,8 @@ help([[
          PyTorch machine learning library for Python with Horovod support.
 ]])
 
+family("python_ml_env")
+
 depends_on("gcc/8.3.0")
 depends_on("hpcx-mpi/2.4.0")
 
@@ -11,8 +13,14 @@ local condaRoot = '/appl/soft/ai/miniconda3'
 local envRoot = pathJoin(condaRoot, 'envs', condaEnv)
 local libPath = pathJoin(envRoot, 'lib/python3.7')
 
-prepend_path('PATH', pathJoin(condaRoot, 'condabin'))
 prepend_path('PATH', pathJoin(envRoot, 'bin'))
+prepend_path('PATH', pathJoin(condaRoot, 'condabin'))
 prepend_path('PYTHONPATH', pathJoin(libPath))
 prepend_path('PYTHONPATH', pathJoin(libPath, 'lib-dynload'))
 prepend_path('PYTHONPATH', pathJoin(libPath, 'site-packages'))
+
+setenv('CONDA_EXE', pathJoin(condaRoot, '/bin/conda'))
+setenv('CONDA_DEFAULT_ENV', condaEnv)
+setenv('CONDA_PREFIX', envRoot)
+setenv('CONDA_SHLVL', '2')
+setenv('CONDA_PREFIX_1', '/appl/soft/ai/miniconda3')
