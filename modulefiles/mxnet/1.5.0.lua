9630b9c6461f4f3cb0211c2454af89422b4cd75c
Markus Koskela <markus.koskela@csc.fi>
2019-07-30 15:43:52 +0300

    Add modulefile for mxnet/1.5.0

---
+++ modulefiles/mxnet/1.5.0.lua
@@ -0,0 +1,23 @@
+local condaEnv = 'mxnet-1.5.0'
+
+help([[
+         MXNet deep learning library for Python
+]])
+
+family("python_ml_env")
+
+local condaRoot = '/appl/soft/ai/miniconda3'
+local envRoot = pathJoin(condaRoot, 'envs', condaEnv)
+local libPath = pathJoin(envRoot, 'lib/python3.7')
+
+prepend_path('PATH', pathJoin(envRoot, 'bin'))
+prepend_path('PATH', pathJoin(condaRoot, 'condabin'))
+prepend_path('PYTHONPATH', pathJoin(libPath))
+prepend_path('PYTHONPATH', pathJoin(libPath, 'lib-dynload'))
+prepend_path('PYTHONPATH', pathJoin(libPath, 'site-packages'))
+
+setenv('CONDA_EXE', pathJoin(condaRoot, '/bin/conda'))
+setenv('CONDA_DEFAULT_ENV', condaEnv)
+setenv('CONDA_PREFIX', envRoot)
+setenv('CONDA_SHLVL', '2')
+setenv('CONDA_PREFIX_1', '/appl/soft/ai/miniconda3')
