c814d8e94327280cd82d8c27060b210658fdab03
Mats Sjöberg <mats.sjoberg@csc.fi>
2019-08-29 12:28:10 +0300

    Updated modulefiles to new scheme which removes conda binary

--- modulefiles/pytorch/1.0.1.lua
+++ modulefiles/pytorch/1.0.1.lua
@@ -1,8 +1,10 @@
 local condaEnv = 'pytorch-1.0.1'
+local condaList = subprocess('/appl/soft/ai/miniconda3/condabin/conda list -n ' .. condaEnv)
 
 help([[
          PyTorch machine learning library for Python
-]])
+
+]] .. condaList)
 
 family("python_ml_env")
 
@@ -11,13 +13,7 @@ local envRoot = pathJoin(condaRoot, 'envs', condaEnv)
 local libPath = pathJoin(envRoot, 'lib/python3.7')
 
 prepend_path('PATH', pathJoin(envRoot, 'bin'))
-prepend_path('PATH', pathJoin(condaRoot, 'condabin'))
+
 prepend_path('PYTHONPATH', pathJoin(libPath))
 prepend_path('PYTHONPATH', pathJoin(libPath, 'lib-dynload'))
 prepend_path('PYTHONPATH', pathJoin(libPath, 'site-packages'))
-
-setenv('CONDA_EXE', pathJoin(condaRoot, '/bin/conda'))
-setenv('CONDA_DEFAULT_ENV', condaEnv)
-setenv('CONDA_PREFIX', envRoot)
-setenv('CONDA_SHLVL', '2')
-setenv('CONDA_PREFIX_1', '/appl/soft/ai/miniconda3')
