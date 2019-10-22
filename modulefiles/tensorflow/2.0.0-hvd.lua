095fa9e19c46ccccd6862fc73025a44b722b45ba
Markus Koskela <markus.koskela@csc.fi>
2019-10-22 13:51:01 +0300

    Add modulefile for tf/2.0.0-hvd

---
+++ modulefiles/tensorflow/2.0.0-hvd.lua
@@ -0,0 +1,24 @@
+local condaEnv = 'tensorflow-hvd-2.0.0'
+
+help([[
+Tensorflow deep learning library for Python
+
+For more help see: https://docs.csc.fi/#apps/tensorflow/]])
+
+family("python_ml_env")
+
+depends_on("gcc/8.3.0")
+depends_on("hpcx-mpi/2.4.0")
+
+local condaRoot = '/appl/soft/ai/miniconda3'
+local envRoot = pathJoin(condaRoot, 'envs', condaEnv)
+local libPath = pathJoin(envRoot, 'lib/python3.7')
+
+prepend_path('PATH', pathJoin(envRoot, 'bin'))
+prepend_path('PATH', '/appl/soft/ai/bin')
+
+prepend_path('PYTHONPATH', pathJoin(libPath, 'site-packages'))
+prepend_path('PYTHONPATH', pathJoin(libPath, 'lib-dynload'))
+prepend_path('PYTHONPATH', pathJoin(libPath))
+
+setenv('CONDA_DEFAULT_ENV', condaEnv)
